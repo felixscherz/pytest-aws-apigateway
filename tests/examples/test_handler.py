4849236d612564c6734d37ab9ba2df939d6a956b
Felix Scherz <felixwscherz@gmail.com>
2024-05-12 21:04:44 +0200

    test: testing output transformation from dict to httpx.Response

--- tests/examples/test_handler.py
+++ tests/examples/test_handler.py
@@ -16,7 +16,7 @@ def test_handler(apigateway: ApiGateway):
         assert resp.json() == {"body": "hello"}
 
 
-def test_invalid_output_format(apigateway: ApiGateway):
+def test_invalid_output_format_returns_500(apigateway: ApiGateway):
     def handler(event, context):
         return {"statusCode": "200"}
 
@@ -27,3 +27,16 @@ def test_invalid_output_format(apigateway: ApiGateway):
     with httpx.Client() as client:
         resp = client.get("https://some/")
         assert resp.status_code == 500
+
+
+def test_output_dict_is_transformed_to_response(apigateway: ApiGateway):
+    def handler(event, context):
+        return {"statusCode": 200}
+
+    apigateway.add_integration(
+        "/", handler=handler, method="GET", endpoint="https://some/"
+    )
+
+    with httpx.Client() as client:
+        resp = client.get("https://some/")
+        assert resp.status_code == 200
