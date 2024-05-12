d0f9a0bdd27b6b12997aa1e150bb360626227236
Felix Scherz <felixwscherz@gmail.com>
2024-05-12 16:51:17 +0200

    feat: implement a basic LambdaContext object

--- src/pytest_aws_apigateway/apigateway.py
+++ src/pytest_aws_apigateway/apigateway.py
@@ -1,14 +1,15 @@
-from typing import Callable
-import httpx
 import json
-import pytest_httpx
 import re
+from typing import Callable
+from typing import Union
 
-from pytest_aws_apigateway.event import (
-    OutputFormatError,
-    request_to_event,
-    transform_response,
-)
+import httpx
+import pytest_httpx
+
+from pytest_aws_apigateway.context import LambdaContext, create_context
+from pytest_aws_apigateway.event import OutputFormatError
+from pytest_aws_apigateway.event import request_to_event
+from pytest_aws_apigateway.event import transform_response
 
 __all__ = ["ApiGateway"]
 
@@ -19,21 +20,20 @@ class ApiGateway:
         ...
 
     def add_integration(
-        self, resource: str, method: str, handler: Callable, endpoint: str
+        self,
+        resource: str,
+        method: str,
+        endpoint: str,
+        handler: Callable[[dict, LambdaContext], Union[dict, httpx.Response]],
     ):
-        p = re.compile(r"\{([^\/]+)\}")
-        res = re.subn(p, r"([^\/]+)", resource)
-        if res[1] > 0:
-            newp = re.compile(f"{endpoint}/{res[0]}")
-        else:
-            newp = endpoint
+        resource = self._normalize_resource(resource)
 
-        resource = resource.lstrip("/")
-        resource = f"/{resource}"
+        url = self._url_expression(endpoint, resource)
 
         def integration(request: httpx.Request) -> httpx.Response:
             event = request_to_event(request, resource)
-            resp = handler(event, None)
+            context = create_context(handler)
+            resp = handler(event, context)
             try:
                 return transform_response(resp)
             except OutputFormatError:
@@ -42,4 +42,17 @@ class ApiGateway:
                     json=json.dumps({"message": "Internal server error"}),
                 )
 
-        self.httpx_mock.add_callback(callback=integration, url=newp, method=method)
+        self.httpx_mock.add_callback(callback=integration, url=url, method=method)
+
+    def _normalize_resource(self, resource: str) -> str:
+        resource = resource.lstrip("/")
+        resource = f"/{resource}"
+        return resource
+
+    def _url_expression(self, endpoint: str, resource: str) -> Union[re.Pattern, str]:
+        p = re.compile(r"\{([^\/]+)\}")
+        res = re.subn(p, r"([^\/]+)", resource)
+        if res[1] > 0:
+            return re.compile(f"{endpoint}/{res[0]}")
+        else:
+            return endpoint
